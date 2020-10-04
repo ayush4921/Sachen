339bc776dd2a8d89a2560a64b977103f334b4155
kuchenluege <71273405+kuchenluege@users.noreply.github.com>
2020-10-03 22:48:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, flash, request, redirect, url_for, render_template
 import pandas as pd
 from datetime import date
+import random
 
 today = date.today()
 #list123=["sfg",'sgsrg','aegaeg','agaeg']
@@ -10,32 +11,52 @@ today = date.today()
 #drawer2=["sfg",'sgsrg','aegaeg','agaeg']
 #drawer3=["sfg",'sgsrg','aegaeg','agaeg']
 
-Milk = {'RFID': "49 20 B3 B0",
-'Item': 'Milk', 
-'Expiration_date': 10052020, #the number is month, day, year
-'Cabinet_number': 5,
-'In Cabinet': False
-}
-Bread = {'RFID': "24 2E BE 2B",
-'Item':'Bread',
-'Expiration_date': 10202020, #the number is month, day, year
-'Cabinet_number': 3,
-'In Cabinet': True
-}
-Eggs = {'RFID': "78 2E BE 2B",
-'Item':'Eggs',
-'Expiration_date': 10152020,
-'Cabinet_number': 8,
-'In Cabinet': False
-}
-df=pd.DataFrame([Milk,Bread,Eggs])
+class inventoryItem:
+    def __init__(self, name, id, expiration_date, cabinet_number, inside_cabinet):
+        self.id = id
+        self.name = name
+        self.expiration_date = expiration_date
+        self.cabinet_number = cabinet_number
+        self.inside_cabinet = inside_cabinet
 
-d1 = today.strftime("%d%m%Y")
-print(d1)
-reminders=[]
-for ind in df.index: 
-     if (df['Expiration_date'][ind]-int(d1))>1000000:
-       df['About to expire']="True"
+#Milk = inventoryItem('Milk', '49 20 B3 B0', 10052020, 5, False)
+#Bread = inventoryItem('Bread', '24 2E BE 2B', 10202020, 3, True)
+#Eggs = inventoryItem('Eggs', '78 2E BE 2B', 10152020, 8, False)
+hex_digits = '0 1 2 3 4 5 6 7 8 9 A B C D E F'.split()
+perishables = 'Milk Bread Eggs Tomatoes Apples Bananas Bacon Broccoli Cheese Cake'.split()
+
+data = {}
+for i in range(0, 100):
+    id = ''
+    id_spaces = ''
+    for j in range (0, 4):
+        digs = hex_digits[random.randint(0, 15)] + hex_digits[random.randint(0, 15)]
+        id += digs
+        id_spaces += digs
+        if j != 4:
+            id_spaces += ' '
+    data[id] = inventoryItem(perishables[random.randint(0, 9)], id_spaces, date.fromordinal(random.randint(737690, 738055)), random.randint(1, 10), bool(random.getrandbits(1)))
+
+data_sorted = sorted(data, key = lambda rfid: data[rfid].expiration_date)
+
+item_list = []
+for key in data_sorted:
+    item_obj = data[key]
+    item_list.append(
+        {
+            'RFID': item_obj.id,
+            'Item': item_obj.name,
+            'Expiration Date': item_obj.expiration_date,
+            'Cabinet Number': item_obj.cabinet_number,
+            'In Cabinet?': item_obj.inside_cabinet
+        }
+    )
+df=pd.DataFrame(item_list)
+
+reminders = []
+for ind in df.index:
+    print((df['Expiration Date'][ind] - today).days)
+    df['About to Expire?'] = (df['Expiration Date'][ind] - today).days < 7
 
 
 
